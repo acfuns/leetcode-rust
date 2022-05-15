e1c57d3e2137130d589bfcca5a8c32056571842a
acfuns <1haolong.sun@gmail.com>
2022-05-15 14:54:11 +0800

    chore: add doc

--- src/easy/s0812_largest_triangle_area.rs
+++ src/easy/s0812_largest_triangle_area.rs
@@ -1,15 +1,46 @@
 use std::cmp::max;
 
-// cross product
+/// It returns the cross product of two vectors
+///
+/// Arguments:
+///
+/// * `x1`: x coordinate of the first point
+/// * `y1`: The y-coordinate of the first point.
+/// * `x2`: i32, y2: i32
+/// * `y2`: i32, x2: i32, y1: i32, x1: i32
+///
+/// Returns:
+///
+/// The cross product of the two vectors.
 fn cross(x1: i32, y1: i32, x2: i32, y2: i32) -> i32 {
     x1 * y2 - y1 * x2
 }
 
 // get area
+/// It takes three points and returns the area of the triangle formed by them
+///
+/// Arguments:
+///
+/// * `a`: &Vec<i32> - a reference to a vector of i32s
+/// * `b`: &Vec<i32> - a reference to a vector of i32s
+/// * `c`: &Vec<i32>
+///
+/// Returns:
+///
+/// The area of the triangle formed by the three points.
 fn area(a: &Vec<i32>, b: &Vec<i32>, c: &Vec<i32>) -> i32 {
     cross(b[0] - a[0], b[1] - a[1], c[0] - a[0], c[1] - a[1])
 }
 
+/// For each triplet of points, compute the area of the triangle formed by them and return the maximum
+///
+/// Arguments:
+///
+/// * `points`: a list of points, each point is a list of two integers
+///
+/// Returns:
+///
+/// The area of the largest triangle that can be formed from the given points.
 pub fn largest_triangle_area(points: Vec<Vec<i32>>) -> f64 {
     let mut res = 0;
 
